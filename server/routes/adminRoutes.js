8cd54616d5aaf0c13f518c5e66bb8a69bf2321b4
Khushi <ks2004144@gmail.com>
2025-07-01 14:25:02 +0530

    feat: create API for admin dashboard data

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { adminLogin } from "../controllers/adminController.js";
+import {
+  adminLogin,
+  approveCommentById,
+  deleteCommentById,
+  getAllBlogsAdmin,
+  getAllComments,
+  getDashboard,
+} from "../controllers/adminController.js";
+import auth from "../middleware/auth.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.get("/comments", getAllComments);
+adminRouter.get("/blog", getAllBlogsAdmin);
+adminRouter.post("/delete-comment", auth, deleteCommentById);
+adminRouter.post("/approve-comment", auth, approveCommentById);
+adminRouter.get("/dashboard", auth, getDashboard);
 
 export default adminRouter;
 // This file defines the admin routes for the application.
